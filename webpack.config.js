e753dd433e69d880ec6fc268eca3cbc03d1f36ee
keidrun <kssk96@gmail.com>
2017-11-26 22:58:33 +0900

    Add editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const PATH = {
+  INDEX: {
+    JS: './src/components/index.jsx',
+    CSS: './src/styles/index.css',
+    HTML: './src/index.html',
+  },
+  DIST: path.join(__dirname, 'public'),
+};
 const VENDOR_LIBS = ['react', 'react-dom'];
 
 module.exports = {
   entry: {
-    bundle: ['./src/components/index.jsx', './src/styles/index.css', './src/index.html'],
+    bundle: [PATH.INDEX.JS, PATH.INDEX.CSS, PATH.INDEX.HTML],
     vendor: VENDOR_LIBS,
   },
   output: {
-    path: path.join(__dirname, 'public'),
+    path: PATH.DIST,
     filename: '[name].[hash].js',
   },
   module: {
@@ -56,14 +64,14 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest'],
     }),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: PATH.INDEX.HTML,
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
@@ -74,7 +82,7 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     hot: true,
-    contentBase: './build',
+    contentBase: PATH.DIST,
     port: 8080,
     inline: true,
     historyApiFallback: true,
